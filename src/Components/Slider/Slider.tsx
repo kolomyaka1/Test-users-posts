c0d11d7979722878d2a990bac52de27aaeb3f172
kolomyaka1 <66018338+kolomyaka1@users.noreply.github.com>
2022-04-03 17:59:49 +0300

    Создание slider

--- src/Components/Slider/Slider.tsx
+++ src/Components/Slider/Slider.tsx
@@ -1,36 +1,29 @@
 import React from "react";
 import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import './Slider.scss';
 
 type Props = {};
 
 const SliderComponent = (props: Props) => {
   const settings = {
-    dots: true,
     infinite: true,
     speed: 500,
     slidesToShow: 4,
     slidesToScroll: 1,
   };
-  return <div>
-    <Slider {...settings}>
-          <div>
-            <h3>1</h3>
-          </div>
-          <div>
-            <h3>2</h3>
-          </div>
-          <div>
-            <h3>3</h3>
-          </div>
-          <div>
-            <h3>4</h3>
-          </div>
-          <div>
-            <h3>5</h3>
-          </div>
-          <div>
-            <h3>6</h3>
-          </div>
+  return <div className='slider'>
+        <Slider {...settings}>
+          {[1,2,3,4,5,6,7,8,9].map((item,index) => {
+            return (
+              <div key={index}>
+              <h1 style={{ color: 'red', fontSize: '48px', textAlign: 'center'}}>
+                {item}
+              </h1>
+            </div>
+            )
+          })}
         </Slider>
   </div>;
 };
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import createRoot from 'react-dom';
 import App from './App';
 
+
 createRoot.render(
     <React.StrictMode>
         <App />
