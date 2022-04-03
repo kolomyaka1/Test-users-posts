d61b96f331196c3d8983ed429e30b82dbc174065
kolomyaka1 <66018338+kolomyaka1@users.noreply.github.com>
2022-04-03 16:52:51 +0300

    create app

---
+++ src/Components/Slider/Slider.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+type Props = {};
+
+const Slider = (props: Props) => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+  return <div>
+
+  </div>;
+};
+
+export default Slider;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import s from "./Home.module.scss";
+
+type Props = {};
+
+const Home = (props: Props) => {
+  return (
+    <div className={s.wrapper}>
+      <div className={s.header_title}>
+        <h2 className={s.top_title}>Наши топ-блогеры</h2>
+        <h3 className={s.bot_title}>
+          Лучше специалисты в своем деле,<br/> средний опыт работы в профессии - 27
+          лет
+        </h3>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
